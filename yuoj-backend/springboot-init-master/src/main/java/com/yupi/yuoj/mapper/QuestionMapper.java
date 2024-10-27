9dfa0431edef3f094f806e82a231a2f025461406
ahyza <2518650772@qq.com>
2024-10-27 11:25:16 +0800

    提交文件

---
+++ yuoj-backend/springboot-init-master/src/main/java/com/yupi/yuoj/mapper/QuestionMapper.java
@@ -0,0 +1,14 @@
+package com.yupi.yuoj.mapper;
+
+import com.yupi.yuoj.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
---
+++ yuoj-backend/springboot-init-master/src/main/java/com/yupi/yuoj/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,14 @@
+package com.yupi.yuoj.mapper;
+
+import com.yupi.yuoj.model.entity.QuestionSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
